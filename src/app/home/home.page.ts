26f24ab5f51822c41d05c6085a407abafedda1b1
chicho86 <sergio.milici@gmail.com>
2020-06-22 13:33:11 -0300

    Add map with scroll issue

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: [
+    'home.page.scss',
+    'map.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
-  constructor() {}
+  constructor() {
+  }
+
+  ngAfterViewInit() {
+    window.mapboxgl.accessToken = '<YOUR_ACCESS_TOKEN>';
+    const map = new window.mapboxgl.Map({
+      container: 'map', // container id
+      style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9 // starting zoom
+    });
+
+    map.once('load', () => map.resize());
+  }
 
 }
